1934806f4594059284a95c010809a712db06525e
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-06-11 09:18:33 -0400

    Added login endpoint in authRoutes.js

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-//Step 1 - Task 2: Import necessary packages
+//Import necessary packages
 const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
@@ -8,24 +8,28 @@ const connectDatabase =require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const Pino = require('pino')
-//Step 1 - Task 3: Create a Pino logger instance
+// Create a Pino logger instance
 const logger = Pino(); 
 
 dotenv.config();
 
-//Step 1 - Task 4: Create JWT secret
+// Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 //Implement the /register endpoint 
 router.post('/register', async (req, res) => {
     //console.log("Register successfully ")
     //console.log('Request body',req.body)
     try {
-        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        // Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
          const  db = await connectDatabase()
-        // Task 2: Access MongoDB collection
+        //  Access MongoDB collection
          const collection = db.collection('users')
-        //Task 3: Check for existing email
+        // Check for existing email
          const existingEmail = await collection.findOne({email : req.body.email});
+         if(existingEmail){
+            logger.error('Email id already exits')
+            return res.status(400).json({error: 'Email already exit'})
+         }
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
@@ -37,15 +41,54 @@ router.post('/register', async (req, res) => {
         password:hash,
         createdAt: new Date()
        })
-        //Task 5: Create JWT authentication with user._id as payload
+        // Create JWT authentication with user._id as payload
         const payload = {
             user: {
                 id: newUser.insertedId,
             },
         };
+        //created Jwt token
         const authtoken = jwt.sign(payload,JWT_SECRET);
         logger.info('User registered successfully');
         res.json({authtoken,email});
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+    }
+});
+//login
+router.post('/login', async (req, res) => {
+    try {
+        //  Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectDatabase();
+        //  Access MongoDB `users` collection
+        const collection = db.collection('users');
+        //  Check for user credentials in database
+        const theUser = await collection.findOne({email: req.body.email});
+        // Check if the password matches the encrypyted password and send appropriate message on mismatch
+        if(theUser){
+            let result = await bcryptjs.compare(req.body.password , theUser.password);
+            if(!result){
+                logger.error('passwords do not match')
+                return res.status(404).json({error: 'wrong password'})
+            }
+            // Create JWT authentication if passwords match with user._id as payload
+            let payload = {
+                user:{
+                id: theUser._id.toString(),
+            },
+        };
+        //  Fetch user details from database
+        const userName = theUser.userName;
+        const userEmail = theUser.email;
+         // Create JWT authentication if passwords match with user._id as payload
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info("user logged in successfully ")
+        return res.status(200).json({authtoken,userName,userEmail})
+      } else {
+        //Send appropriate message if user not found
+        logger.error("User not found")
+        return res.status(404).json({error:'User not found'})
+      };
     } catch (e) {
          return res.status(500).send('Internal server error');
     }
