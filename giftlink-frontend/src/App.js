716cedbb6a05255d0ee6051ebf795951fb396ce0
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-06-11 16:51:13 -0400

    Implemented Backend in profile component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
 function App() {
     //const navigate = useNavigate();
@@ -22,6 +23,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element ={<RegisterPage/>}/>
           <Route path="/app/search" element ={<SearchPage/>}/>
+          <Route path="/app/profile" element ={<Profile/>}/>
           
         </Routes>
         </>
